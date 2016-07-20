f2ae136d07681fc7083e7d89e0454e8bd2eb709b
odholden <odholden@gmail.com>
2016-07-20 15:32:26 +0100

    adapted webhook handler from github to allow for use in this app, about to start parsing webhooks into the database

--- controllers/pushController.js
+++ controllers/pushController.js
@@ -1,11 +1,22 @@
-var util = require("util");
-
 var Push = require("../models/push");
+var createHandler = require("../config/webhookHandler");
+var handler = createHandler({ path: "/", secret: "rodney"});
 
 function create(req, res) {
   console.log("request happening =============================");
-  console.log(util.inspect(req.read, false, null));
-  return res.end("request successfully received!");
+
+  handler(req, res, function (err) {
+    console.log(err);
+  });
+
+  return res.end("Webhook payload received!");
 }
 
 module.exports = { create: create };
+
+handler.on('push', function (event) {
+  console.log("push detected =================================");
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref);
+});
