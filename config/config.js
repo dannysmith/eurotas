6e8b0ec793ce0bffd0ef6b101f34d5b159ae0f56
odholden <odholden@gmail.com>
2016-07-26 09:05:23 +0100

    changed mongolab uri to mongodb uri

--- config/config.js
+++ config/config.js
@@ -4,5 +4,5 @@ module.exports = {
     "destination": process.env.GITHUB_DESTINATION_REPO
   },
   "port": process.env.PORT || 3000,
-  "database": process.env.MONGOLAB_URI || process.env.DATABASE
+  "database": process.env.MONGODB_URI
 };
