a82ee6c6a0841daaceb73d571a4becdc16a4a2de
odholden <odholden@gmail.com>
2016-08-01 17:39:13 +0100

    blah

--- controllers/input.js
+++ controllers/input.js
@@ -64,6 +64,8 @@ function runBash(origin, message) {
                    " cd " + temp + " &&" + 
                    " git clone https://" + userPass + "@github.com/" + origin + ".git &&" + 
                    " cd " + repo1 + " &&" +
+                   ' git config user.email "odholden@gmail.com" &&' +
+                   ' git config user.name "Oliver Holden" &&' +
                    " git remote rm origin &&" +
                    " mkdir imported &&" +
                    " mv * imported || true &&" +
@@ -74,6 +76,8 @@ function runBash(origin, message) {
                    " rm -rf "+ repo2 +"/* &&" +
                    " cp -R " + repo1 + "/* " + repo2 + " &&" +
                    " cd " + repo2 + " &&" +
+                   ' git config user.email "odholden@gmail.com" &&' +
+                   ' git config user.name "Oliver Holden" &&' +
                    " git add -A &&" +
                    " git commit -m '" + message + "' &&" +
                    ' echo "machine github.com login ' + username + ' password ' + password + '" >> ~/.netrc &&' +
